e045c353fcc0c6f5df5ba1ad563d1e657364dcf3
alykkehoy <alykkeho@purdue.edu>
2017-10-16 07:01:39 -0400

    finished lab6

--- Lab06/Lab06.sql
+++ Lab06/Lab06.sql
@@ -5,6 +5,20 @@
 -- 1. Display the name and city of customers who live in any city that makes the most
 -- different kinds of products. (There are two cities that make the most different
 -- products. Return the name and city of customers from either one of those.)
+SELECT name, city
+FROM customers
+WHERE city IN (
+  SELECT city
+  FROM products as p1
+  GROUP BY p1.city
+  HAVING count(p1.pid) = (
+    SELECT count(p2.pid)
+    FROM products AS p2
+    GROUP BY p2.city
+    ORDER BY count(p2.pid) DESC
+    LIMIT 1
+  )
+);
 
 -- 2. Display the names of products whose priceUSD is at or above the average priceUSD, in
 -- reverse-alphabetical order.
@@ -23,9 +37,7 @@ ORDER BY totalusd ASC;
 
 -- 4. Display all customer names (in reverse alphabetical order) and their total ordered,
 -- and nothing more. Use coalesce to avoid showing NULLs.
-
--- TODO
-SELECT customers.name, sum(totalUSD)
+SELECT customers.name, coalesce(sum(totalUSD), 0)
 FROM orders RIGHT OUTER JOIN customers ON orders.cid = customers.cid
 GROUP BY customers.cid
 ORDER BY customers.name DESC;
@@ -50,3 +62,11 @@ WHERE orders.totalUSD <> (products.priceusd * orders.quantity * (1 - (customers.
 -- 7. What’s the difference between a LEFT OUTER JOIN and a RIGHT OUTER JOIN? Give
 -- example queries in SQL to demonstrate. (Feel free to use the CAP database to make
 -- your points here.)
+
+-- The difference between right outer join and left outer join is the order the tables are joined
+-- in. For a left outer join, the right table is joined into the left one. Where there is no match
+-- in the right table, NULL is entered. This works the opposit way for a right outer join.
+
+-- If you were to say "customers left outer join orders on cusotmers.cid = orders.cid", all orders
+-- would be matched to their customers, but if there existed a customer who made no orders the fields
+-- would left NULL
